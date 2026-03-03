ba3fd4bfb1fa37a444e0e64a26f126868524c37a
Phil Peterson <phpeterson@usfca.edu>
2026-03-03 14:23:46 -0800

    add 3/3 s02

---
+++ week06/section02/machine_code/add2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int add2_s(int, int);
+
+int main(int argc, char **argv) {
+    uint32_t *instructions = (uint32_t*) add2_s;
+    uint32_t instr = *instructions;
+    
+    printf("first instr: %x\n", instr);
+        
+}
